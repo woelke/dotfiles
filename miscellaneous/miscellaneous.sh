ba9a577c223d5312d960dba53a50236abd87922d
Your Name <you@example.com>
2015-11-19 11:11:18 +0100

    Add termintor config to mescellaneous.

--- miscellaneous/miscellaneous.sh
+++ miscellaneous/miscellaneous.sh
@@ -3,6 +3,7 @@
 if [ "$1" = "all" ]; then
     ./miscellaneous.sh xresources 
     ./miscellaneous.sh nautilus 
+    ./miscellaneous.sh terminator 
 fi
 
 if [ "$1" = "xresources" ]; then
@@ -28,6 +29,32 @@ Name=gvim
 Terminal=true
 Type=Application
 Version=1.0
+EOF
+
+fi
+
+if [ "$1" = "terminator" ]; then  
+    mkdir ~/.config/terminator
+    cd ~/.config/terminator
+
+cat > config <<EOF 
+[global_config]
+[keybindings]
+[profiles]
+  [[default]]
+    scrollback_lines = 20000
+    font = Monospace 12
+    use_system_font = False
+    background_image = None
+[layouts]
+  [[default]]
+    [[[child1]]]
+      type = Terminal
+      parent = window0
+    [[[window0]]]
+      type = Window
+      parent = ""
+[plugins]
 EOF
 
 fi
