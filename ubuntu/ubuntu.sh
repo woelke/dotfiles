c8b6b8dc0aa786dc07e15dc4744cf045fcba626d
aufgang001 <aufgang001@aufgang001.com>
2017-07-31 09:12:19 +0200

    Remove annoying system sounds

--- ubuntu/ubuntu.sh
+++ ubuntu/ubuntu.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 if [ "$1" = "all" ]; then
-  : #null command
+  ./ubuntu.sh system_sounds
+fi
+
+if [ "$1" = "system_sounds" ]; then
+  cd /usr/share/sounds/freedesktop/stereo
+  sudo mkdir backup
+  sounds="audio-volume-change.oga" 
+
+  for sound in $sounds; do
+    sudo mv $sound backup/.
+  done
 fi
 
