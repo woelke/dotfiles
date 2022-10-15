47e31ed055c52fb6ec53faaa87e3e2bffb46321f
Sebastian Woelke <sebastian.woelke@posteo.de>
2022-10-15 18:53:30 +0200

    Update config for peco

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 set -eu -o pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-BIN_DIR="${HOME}/.local/bin/"
+BIN_DIR="${HOME}/.local/bin"
 
 PATH_STR='${PATH}'
 echo "export PATH=${BIN_DIR}:${PATH_STR}" >> "${HOME}/.profile"
