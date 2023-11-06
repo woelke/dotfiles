7b9e7af07caf938a9921cfbb1f430ec5a7de12b0
Sebastian Woelke <sebastian.woelke@posteo.de>
2023-11-06 19:51:53 +0100

    add .local/bin to path

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,13 @@
 set -eu -o pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+BIN_DIR="${HOME}/.local/bin"
 
+PATH_STR='${PATH}'
+echo "export PATH=${BIN_DIR}:${PATH_STR}" >> "${HOME}/.profile"
 echo "export DOTFILES_ROOT=${SCRIPT_DIR}" >> "${HOME}/.profile"
+
+echo "export PATH=${BIN_DIR}:${PATH_STR}" >> "${HOME}/.yprofile"
 echo "export DOTFILES_ROOT=${SCRIPT_DIR}" >> "${HOME}/.zprofile"
 
 pushd dotfiles_py
